22b0df52ff21c652e48d59a54980bcaba73e53ef
Peter Berck <pberck@gmail.com>
2013-12-16 16:35:26 +0100

    Adapted for Mac.

--- do_compile.bash
+++ do_compile.bash
@@ -6,7 +6,8 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     echo "cp liblin.xpl.so /x/games/X-Plane10/Resources/plugins/gpxlog/64/lin.xpl"
     cp liblin.xpl.so /x/games/X-Plane10/Resources/plugins/gpxlog/64/lin.xpl 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-        echo "Macintosh"
+        echo "cp libmac.xpl.dylib /Users/pberck/priv/X-Plane\ 10/Resources/plugins/GPXlog/"
+	cp libmac.xpl.dylib /Users/pberck/priv/X-Plane\ 10/Resources/plugins/GPXlog/
 elif [[ "$OSTYPE" == "win32" ]]; then
         echo "Windows"
 else
